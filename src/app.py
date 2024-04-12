6b52bb65a3251c05aedbb8488469847535ab0e1c
Pablo Robles <156269638+PabloRoblesA@users.noreply.github.com>
2024-04-12 03:35:24 +0000

    API spotify/ spotipy Probles

--- src/app.py
+++ src/app.py
@@ -1,7 +1,24 @@
 import os
-import pandas as pd
-import seaborn as sns
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # load the .env file variables
 load_dotenv()
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
+SP = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri='https://localhost'))
+
+ID_ARTISTA ='2ziB7fzrXBoh1HUPS6sVFn'
+response = SP.artist_top_tracks(ID_ARTISTA)
+
+TRACKS = response['tracks']
+df_tracks = pd.DataFrame.from_records(TRACKS)
+df_tracks_sort = df_tracks.sort_values(by=['popularity'])
+#print(df_tracks_sort.head(3))
+
+plt.scatter(x=df_tracks['popularity'],y=df_tracks['duration_ms'])
+plt.show
